b367a019f8a69ca3eca5256fb624c6e9b2e5a893
shopcard <160701623+shopcard@users.noreply.github.com>
2024-02-22 10:24:19 +0700

    Create reactstuff.js

---
+++ reactstuff.js
@@ -0,0 +1,40 @@
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  localWallet,
+  embeddedWallet,
+} from "@thirdweb-dev/react";
+
+export default function App() {
+  return (
+    <ThirdwebProvider
+      activeChain="mumbai"
+      clientId="YOUR_CLIENT_ID"
+      locale={en()}
+      supportedWallets={[
+        metamaskWallet(),
+        localWallet(),
+        embeddedWallet({
+          auth: {
+            options: [
+              "email",
+              "google",
+              "apple",
+              "facebook",
+            ],
+          },
+        }),
+      ]}
+    >
+      <ConnectWallet
+        theme={"dark"}
+        modalSize={"compact"}
+        modalTitleIconUrl={
+          "https://shopcard.github.io/images/LMAO.jpeg"
+        }
+        showThirdwebBranding={false}
+      />
+    </ThirdwebProvider>
+  );
+}
